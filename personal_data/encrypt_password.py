542ab49e92458527375595098eabb1e6dc60bdc0
mnshimiyimana <m.nshimiyim@alustudent.com>
2024-05-24 18:25:20 +0200

    Updated encrypt pass file

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -5,8 +5,10 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
+    """hash password function"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
+    """hash password function"""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
