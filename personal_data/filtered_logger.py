51d5a634f8131f11547650ab0b4fbb99c177e6a6
mnshimiyimana <m.nshimiyim@alustudent.com>
2024-05-24 18:22:51 +0200

    Updates

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 """ Regex-ing"""
 
+import logging
 import re
 from typing import List
+import mysql.connector
+from os import getenv
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
 
 def filter_datum(fields: List[str],
                  redaction: str,
@@ -13,3 +19,68 @@ def filter_datum(fields: List[str],
         message = re.sub(field + "=.*?" + separator,
                          field + "=" + redaction + separator, message)
     return message
+    
+
+class RedactingFormatter(logging.Formatter):
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        msg = filter_datum(self.fields, self.REDACTION,
+                           super().format(record), self.SEPARATOR)
+        return msg
+
+
+def get_logger() -> logging.Logger:
+    """return logger"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    db = mysql.connector.connection.MySQLConnection(
+        user=getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=getenv('PERSONAL_DATA_DB_NAME'))
+
+    return db
+
+def main():
+    """ Redacting Formatter class."""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+
+    result = cursor.fetchall()
+    for data in result:
+        message = f"name={data[0]}; " + \
+                  f"email={data[1]}; " + \
+                  f"phone={data[2]}; " + \
+                  f"ssn={data[3]}; " + \
+                  f"password={data[4]}; " + \
+                  f"ip={data[5]}; " + \
+                  f"last_login={data[6]}; " + \
+                  f"user_agent={data[7]};"
+        print(message)
+        log_record = logging.LogRecord("my_logger", logging.INFO,
+                                       None, None, message, None, None)
+        formatter = RedactingFormatter(PII_FIELDS)
+        formatter.format(log_record)
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
