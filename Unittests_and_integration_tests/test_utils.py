e6ebac702047d939cae9543eff9bc3807b4a9b78
mnshimiyimana <m.nshimiyim@alustudent.com>
2024-06-14 01:41:03 +0200

    updated test_utils

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
-from parameterized import parameterized # type: ignore
+from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 from unittest.mock import patch
 import requests
