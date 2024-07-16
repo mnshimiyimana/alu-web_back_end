92d9a033b3ce60b78635c891d06991183502f51a
mnshimiyimana <m.nshimiyim@alustudent.com>
2024-07-17 00:31:22 +0200

    added a new line

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,34 +1,34 @@
 /* eslint-disable no-unused-vars */
-import Currency from './3-currency';
+import Currency from "./3-currency";
 /* eslint-enable no-unused-vars */
 
 export default class Pricing {
-    constructor(amount, currency) {
-        this._amount = amount;
-        this._currency = currency;
-    }
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-    get amount() {
-        return this._amount;
-    }
+  get amount() {
+    return this._amount;
+  }
 
-    set amount(newAmount) {
-        this._amount = newAmount;
-    }
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
 
-    get currency() {
-        return this._currency;
-    }
+  get currency() {
+    return this._currency;
+  }
 
-    set currency(newCurrency) {
-        this._currency = newCurrency;
-    }
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
 
-    displayFullPrice() {
-        return `${this._amount} ${this._currency.displayFullCurrency()}`;
-    }
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
 
-    static convertPrice(amount, conversionRate) {
-        return amount * conversionRate;
-    }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
