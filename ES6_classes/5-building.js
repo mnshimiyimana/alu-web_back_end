0992702041775f9a1bae06e216ddcfdd181011ba
mnshimiyimana <m.nshimiyim@alustudent.com>
2024-07-17 00:44:51 +0200

    fixed errors

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,11 +1,10 @@
 export default class Building {
   constructor(sqft) {
     if (
-      this.constructor !== Building &&
-      typeof this.evacuationWarningMessage !== 'function'
+      this.constructor !== Building && typeof this.evacuationWarningMessage !== "function"
     ) {
       throw Error(
-        'Class extending Building must override evacuationWarningMessage'
+        "Class extending Building must override evacuationWarningMessage"
       );
     }
 
