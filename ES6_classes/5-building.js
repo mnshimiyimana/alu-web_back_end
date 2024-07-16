54814c271cc74f2709e64ab781cf22c8f491a8f4
mnshimiyimana <m.nshimiyim@alustudent.com>
2024-07-17 00:51:11 +0200

    added a new line

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,10 +1,12 @@
 export default class Building {
   constructor(sqft) {
     if (
-      this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function'
+      this.constructor !== Building &&
+      typeof this.evacuationWarningMessage !== 'function'
     ) {
       throw Error(
-        'Class extending Building must override evacuationWarningMessage');
+        'Class extending Building must override evacuationWarningMessage'
+      );
     }
     this._sqft = sqft;
   }
